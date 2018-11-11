ed6c8554283ef964fa02cd9178fc7ed8127fc9c0
jxltom <jxltom@gmail.com>
2018-11-11 14:21:05 +0800

    Add graphql support

--- lib/listview.dart
+++ lib/listview.dart
@@ -1,30 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 import 'package:contrihub/fake_data.dart';
+
+final _readPullRequestsGraphql = """
+  query (\$username: String!) {
+    user(login: \$username) {
+      name,
+      bio,
+      pullRequests(
+        first: 50,
+        states: [CLOSED, MERGED, OPEN],
+        orderBy: {
+          field: CREATED_AT,
+          direction: DESC
+        }
+      ) {
+        nodes {
+          state,
+          title,
+          repository {
+            name
+          },
+        }
+      }
+    }
+  }
+"""
+    .replaceAll('\n', ' ');
+
+final _queryPullRequests = Query(
+  _readPullRequestsGraphql,
+  variables: {
+    'username': 'jxltom',
+  },
+  pollInterval: 10,
+  builder: ({
+    bool loading,
+    var data,
+    Exception error,
+  }) {
+    if (error != null) {
+      return Center(
+        child: Text(error.toString()),
+      );
+    }
+
+    if (loading) {
+      return Center(
+        child: Text('Loading'),
+      );
+    }
+
+    List pullRequests = data['user']['pullRequests']['nodes'];
+
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemCount: pullRequests.length,
+        itemBuilder: (context, index) {
+          final pullRequest = pullRequests[index];
+
+          return ListTile(
+            leading: Icon(Icons.merge_type),
+            title: Text(pullRequest['title']),
+            subtitle: Text(pullRequest['title']),
+            trailing: Icon(Icons.comment),
+          );
+        });
+  },
+);
 
 class CustomListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: EdgeInsets.all(16.0),
-      itemBuilder: (BuildContext context, int index) {
-        if (index.isOdd) {
-          return new Divider();
-        }
-
-        if (index > fakeData.length - 1) {
-          index = index % fakeData.length;
-        }
-
-        var data = fakeData.elementAt(index);
-
-        return ListTile(
-          leading: Icon(Icons.merge_type),
-          title: Text(data.project + ' ' + data.title),
-          subtitle: Text(data.description),
-          trailing: Icon(Icons.comment),
-        );
-      },
-    );
+    return _queryPullRequests;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 import 'package:contrihub/homepage.dart';
+
+ValueNotifier<Client> _graphqlClient = ValueNotifier(
+    Client(
+      endPoint: 'https://api.github.com/graphql',
+      cache: InMemoryCache(),
+      apiToken: '',
+    ),
+  );
+
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage(),
-      theme: ThemeData(primaryColor: Colors.white),
+    return GraphqlProvider(
+      client: _graphqlClient,
+      child: MaterialApp(
+        home: HomePage(),
+        theme: ThemeData(primaryColor: Colors.white),
+      ),
     );
   }
 }
