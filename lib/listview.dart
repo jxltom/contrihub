60f8a6477c8d5f3c6d8fc3383d62867256f9f3d1
jxltom <jxltom@gmail.com>
2018-11-13 01:19:54 +0800

    Try to add scroll

--- lib/listview.dart
+++ lib/listview.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 final _readPullRequestsGraphql = """
-  query (\$username: String!) {
+  query (\$username: String!, \$index: Int!) {
     user(login: \$username) {
       pullRequests(
-        first: 50,
+        first: \$index,
         states: [CLOSED, MERGED, OPEN],
         orderBy: {
           field: UPDATED_AT,
@@ -86,11 +86,78 @@ Color _getStateColor(String state) {
   return color;
 }
 
-class CreatedListView extends StatelessWidget {
+final client = Client(
+  endPoint: 'https://api.github.com/graphql',
+  cache: InMemoryCache(),
+  apiToken: '',
+);
+
+class _CreatedListViewState extends State<CreatedListView> {
+  Future<Map<String, dynamic>> query;
+  List<dynamic> _items = List<dynamic>();
+
+  @override
+  void initState() {
+    super.initState();
+
+    query = client.query(
+      query: _readPullRequestsGraphql,
+      variables: {
+        'username': 'jxltom',
+        'index': _items.length + 1,
+      },
+    );
+  }
+
+  Future<Map<String, dynamic>> _getQuery(int index) {
+    return client.query(
+      query: _readPullRequestsGraphql,
+      variables: {
+        'username': 'jxltom',
+        'index': index,
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return _getPullRequestsQuery('jxltom');
+    return FutureBuilder(
+        future: _getQuery(_items.length + 1),
+        builder: (BuildContext context,
+            AsyncSnapshot<Map<String, dynamic>> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(
+              child: CircularProgressIndicator(
+                strokeWidth: 2.0,
+              ),
+            );
+          } else {
+            final d = snapshot.data['user']['pullRequests']['nodes'];
+            //final _pullRequest = d[d.length - 1];
+            _items.add(d[d.length - 1]);
+
+            return ListView.builder(
+              itemBuilder: (context, index) {
+                final pullRequest = _items[index];
+
+                return ListTile(
+                  leading: Icon(Icons.merge_type,
+                      color: _getStateColor(pullRequest['state'])),
+                  title: Text(pullRequest['title']),
+                  subtitle: Text(pullRequest['repository']['nameWithOwner']),
+                  trailing: Icon(Icons.comment),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                );
+              },
+            );
+          }
+        });
   }
+}
+
+class CreatedListView extends StatefulWidget {
+  @override
+  _CreatedListViewState createState() => _CreatedListViewState();
 }
 
 class AssignedListView extends StatelessWidget {
