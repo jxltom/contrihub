e6753ecbc522aa135310205b45c7b058bc7d8adc
jxltom <jxltom@gmail.com>
2018-11-15 00:42:26 +0800

    Add scroll controller

--- lib/pages/pullrequest/pullrequest_tabs.dart
+++ lib/pages/pullrequest/pullrequest_tabs.dart
@@ -10,21 +10,51 @@ import 'package:contrihub/pages/pullrequest/pullrequest_element.dart';
 final _itemsOnce = 3;
 
 class _CreatedPullrequestTabState extends State<CreatedPullrequestTab> {
-  Future<void> _updatePullRequestItemsFuture;
+  int _currentItemsIndex = 0;
   List<dynamic> _pullRequestItems = List<dynamic>();
+  Future<void> _updatePullRequestItemsFuture;
+  ScrollController _scrollController = ScrollController();
 
-  Future<void> _updatePullRequestItems() async {
+  Future<List<dynamic>> _query(int index) async {
     final _data = await graphqlClient.query(
       query: readPullRequestsQuery,
       variables: {
         'username': 'jxltom',
-        'index': _itemsOnce,
+        'index': _currentItemsIndex,
       },
     );
 
+    return _data['user']['pullRequests']['nodes'];
+  }
+
+  Future<void> _resetPullRequestItems() async {
+    _currentItemsIndex = _itemsOnce;
+  
+    final _data = await _query(_currentItemsIndex);
+
     setState(() {
-      _pullRequestItems = _data['user']['pullRequests']['nodes'];
+      _pullRequestItems = _data;
     });
+  }
+
+  Future<void> _updatePullRequestItems() async {
+    print('called');
+    _currentItemsIndex += _itemsOnce;
+  
+    final _data = await _query(_currentItemsIndex);
+
+    setState(() {
+      _pullRequestItems = _data;
+    });
+  }
+
+  // FIXME: Disable loading multiple times
+  bool _onNotification(Notification notification) {
+    if (notification is OverscrollNotification) {
+      _updatePullRequestItems();
+    }
+
+    return true;
   }
 
   @override
@@ -39,6 +69,7 @@ class _CreatedPullrequestTabState extends State<CreatedPullrequestTab> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
+      // FIXME: This is rebuild when the widget is rebuild
       future: _updatePullRequestItemsFuture,
       builder: (BuildContext context, AsyncSnapshot snapshot) {
         // snapshot.hasData doesn't work since the Future returns void
@@ -46,26 +77,31 @@ class _CreatedPullrequestTabState extends State<CreatedPullrequestTab> {
           return LoadingIndicator();
         }
 
-        return RefreshIndicator(
-          onRefresh: _updatePullRequestItems,
-          child: ListView.builder(
-            physics: const AlwaysScrollableScrollPhysics(),
-            padding: EdgeInsets.all(16.0),
-            itemCount: _pullRequestItems.length * 2 + 1,
-            itemBuilder: (context, index) {
-              if (index + 1 == _pullRequestItems.length * 2 + 1) {
-                return LoadingIndicator();
-              }
+        return NotificationListener(
+          onNotification: _onNotification,
+          child: RefreshIndicator(
+            onRefresh: _resetPullRequestItems,
+            child: ListView.builder(
+              physics: const AlwaysScrollableScrollPhysics(),
+              padding: EdgeInsets.all(16.0),
+              controller: _scrollController,
+              itemCount: _pullRequestItems.length * 2 + 1,
+              itemBuilder: (context, index) {
+                // FIXME: Remove loading indicator if items are less than a page
+                if (index + 1 == _pullRequestItems.length * 2 + 1) {
+                  return LoadingIndicator();
+                }
 
-              if (index % 2 == 1) {
-                return Divider();
-              }
+                if (index % 2 == 1) {
+                  return Divider();
+                }
 
-              final pullRequest = _pullRequestItems[index ~/ 2];
-              return PullrequestElement(
-                pullrequest: pullRequest,
-              );
-            },
+                final pullRequest = _pullRequestItems[index ~/ 2];
+                return PullrequestElement(
+                  pullrequest: pullRequest,
+                );
+              },
+            ),
           ),
         );
       },
